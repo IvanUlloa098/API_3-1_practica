f6d591528d010ee121497b1eddf1e840cbcaf409
IvanUlloa098 <iulloa@est.up.edu.ec>
2021-12-23 09:59:12 -0500

    Conexion con MySQL

--- API_contactos/API/models.py
+++ API_contactos/API/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class contactos(models.Model): 
+    id = models.AutoField(primary_key=True, null= False)
+    nombre = models.CharField(max_length=255, blank= False, null= False)
+    numero = models.CharField(max_length=255, blank= False, null= False)
+    
